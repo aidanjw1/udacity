ae0fdc3922e35d833fa750da38c73f14a349a94f
Aidan White <aidan.white36@gmail.com>
2020-01-09 17:02:36 -0600

    navigation: menus and safe args

--- Developing_Android_Apps_with_Kotlin/apps/AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Developing_Android_Apps_with_Kotlin/apps/AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -36,6 +37,11 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
 
         return binding.root
     }
