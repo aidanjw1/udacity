07a33b757c1feacc182294f881c43926cf90ea10
Aidan White <aidan.white36@gmail.com>
2020-01-14 16:59:25 -0600

    begin work on UI architecture module and GuessTheWord app

---
+++ Developing_Android_Apps_with_Kotlin/apps/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,124 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
+
+    private val timer: CountDownTimer
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    private val _eventGameFinished = MutableLiveData<Boolean>()
+    val eventGameFinished: LiveData<Boolean> get() = _eventGameFinished
+
+    private val _secondsRemaining = MutableLiveData<Long>()
+    val secondsRemaining: LiveData<Long> get() = _secondsRemaining
+
+    init {
+        _score.value = 0
+        _word.value = ""
+        _eventGameFinished.value = false
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _secondsRemaining.value = millisUntilFinished / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _eventGameFinished.value = true
+            }
+        }
+        timer.start()
+
+        Log.i("GameViewModel", "GameViewModel created")
+        resetList()
+        nextWord()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            _eventGameFinished.value = true
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+    }
+
+    fun onSkip() {
+        _score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinished.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed")
+        timer.cancel()
+    }
+}
