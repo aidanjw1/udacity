9404b5baa6475a65c18e310af6b3df5867a371e2
Aidan White <aidan.white36@gmail.com>
2020-03-23 13:34:35 -0500

    End of section 10: Updating resources

--- Build an app with ASP.NET Core and Angular from scratch/app/API/Controllers/UsersController.cs
+++ Build an app with ASP.NET Core and Angular from scratch/app/API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.Dtos;
@@ -35,6 +37,21 @@ namespace API.Controllers
             var user = await _repo.GetUser(id);
             var userToReturn = _mapper.Map<UserForDetailsDto>(user);
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto) 
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if(await _repo.saveAll()) 
+                return NoContent();
+            
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
--- Build an app with ASP.NET Core and Angular from scratch/app/SPA/src/app/app.module.ts
+++ Build an app with ASP.NET Core and Angular from scratch/app/SPA/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export class CustomHammerConfig extends HammerGestureConfig {
    overrides = {
@@ -44,7 +47,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -66,6 +70,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       ErrorInterceptorProvider,
       MemberDetailResolver,
       MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
